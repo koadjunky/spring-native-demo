fedd09f88c2893ebacb54db19714342523efa25a
Maciej Malycha <koadjunky@gmail.com>
2022-09-16 07:53:26 +0200

    Initializr template

---
+++ src/test/java/eu/malycha/graalvm/springnativedemo/SpringNativeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.malycha.graalvm.springnativedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNativeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
